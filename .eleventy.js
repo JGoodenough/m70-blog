91eab6c498ae1f2c9105963b2b544c5d44f82df6
Jesse Goodenough <jesse.goodenough@gmail.com>
2022-11-10 10:52:26 -0500

    Add eleventy navigation
    Also about page

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
-module.exports = function (eleventyConfig) {
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
 }
