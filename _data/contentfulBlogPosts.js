b2df009e657247bf1eb93830fb4b117684d0f8aa
Jesse Goodenough <jesse.goodenough@gmail.com>
2022-11-11 15:03:14 -0500

    add blog landing and detail pages

--- _data/contentfulBlogPosts.js
+++ _data/contentfulBlogPosts.js
@@ -2,7 +2,9 @@ const { fetchContentfulEntries } = require('../services/contentful')
 
 module.exports = async function fetchContentfulBlogPosts () {
   try {
-    return fetchContentfulEntries({content_type: 'blogPost'});
+    const posts = await fetchContentfulEntries({content_type: 'blogPost'});
+    console.log(JSON.stringify(posts, null, 2))
+    return posts;
   } catch (error) {
     console.log(`Error from fetchContentfulBlogPosts: ${error}`);
     return 'An error occurred when attempting to fetch the blog posts.'
