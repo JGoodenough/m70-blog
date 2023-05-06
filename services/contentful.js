2346340a664c7b72d1fbab101301ea6fd90490c9
Jesse Goodenough <jesse.goodenough@gmail.com>
2023-05-06 17:04:19 -0400

    Update contentful rich text renderer to display image assets

--- services/contentful.js
+++ services/contentful.js
@@ -1,23 +1,39 @@
 require('dotenv').config();
 const contentful = require('contentful');
 const { documentToHtmlString } = require('@contentful/rich-text-html-renderer');
+const { BLOCKS } = require('@contentful/rich-text-types');
 
 const client = contentful.createClient({
   // This is the space ID. A space is like a project folder in Contentful terms
   space: process.env.CONTENTFUL_SPACE_ID,
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: process.env.CONTENTFUL_PROD_ACCESS_TOKEN
+  accessToken: process.env.CONTENTFUL_PROD_ACCESS_TOKEN,
 });
 
 const fetchContentfulEntries = async (options = {}) => {
   return await client.getEntries(options);
-}
+};
 
 const renderContentfulRichText = (content) => {
-  return documentToHtmlString(content);
-}
+  const renderOptions = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: (node) => {
+        // render the EMBEDDED_ASSET as you need
+        return `<img
+          src=${`https://${node.data.target.fields.file.url}`}
+          height=${node.data.target.fields.file.details.image.height}
+          width=${node.data.target.fields.file.details.image.width}
+          alt=${node.data.target.fields.description}
+        />
+      `;
+      },
+    },
+  };
+  const rendered = documentToHtmlString(content, renderOptions);
+  return rendered;
+};
 
 module.exports = {
   fetchContentfulEntries,
   renderContentfulRichText,
-}
+};
